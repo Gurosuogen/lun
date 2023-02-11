6914d17906aeaa31688640a6bd6bdc3e099d0eb1
gurosuogen <zcw5321@gmail.com>
2023-02-11 22:52:13 +0800

    路由切换之后关闭 aside

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -31,7 +31,9 @@ export default {
                     </li>
                 </ol>
             </aside>
-            <main>主内容</main>
+            <main>
+                <router-view />
+            </main>
         </div>
     </div>
 </template>
