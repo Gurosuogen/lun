bfde1dfdc6582058bcc2aed2d6fafc680934c672
gurosuogen <zcw5321@gmail.com>
2023-02-14 22:22:14 +0800

    支持 level 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -19,10 +19,26 @@
         <Button size="small" theme="link">tiny!</Button>
     </div>
     <div>
-        <Button theme="text" size="big" >It's so big!</Button>
+        <Button theme="text" size="big">It's so big!</Button>
         <Button theme="text">just normal.</Button>
-        <Button theme="text" size="small" >tiny!</Button>
+        <Button theme="text" size="small">tiny!</Button>
     </div>
+    <h1>level示例</h1>
+        <div>
+            <Button level="main">主要按钮</Button>
+            <Button>普通按钮</Button>
+            <Button level="danger">危险按钮</Button>
+        </div>
+        <div>
+            <Button theme="link" level="main">主要链接按钮</Button>
+            <Button theme="link">普通链接按钮</Button>
+            <Button theme="link" level="danger">危险链接按钮</Button>
+        </div>
+        <div>
+            <Button theme="text" level="main">主要文字按钮</Button>
+            <Button theme="text">普通文字按钮</Button>
+            <Button theme="text" level="danger">危险文字按钮</Button>
+        </div>
 </template>
 
 <script lang="ts">
