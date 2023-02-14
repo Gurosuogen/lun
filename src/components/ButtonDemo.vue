fd87a216e84ffc2893f798fb03d5b768922e6a8f
gurosuogen <zcw5321@gmail.com>
2023-02-14 21:17:58 +0800

    更新size的css

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,17 +1,27 @@
 <template>
     <div>Button 示例</div>
-    <h1>示例1</h1>
+    <h1>theme示例</h1>
     <div>
         <Button>hello!</Button>
         <Button theme="button">hello!</Button>
         <Button theme="link">hello!</Button>
         <Button theme="text">hello!</Button>
     </div>
-    <h1>示例2</h1>
+    <h1>size示例</h1>
     <div>
         <Button size="big">It's so big!</Button>
         <Button>just normal.</Button>
         <Button size="small">tiny!</Button>
+    </div>
+    <div>
+        <Button theme="link" size="big">It's so big!</Button>
+        <Button theme="link">just normal.</Button>
+        <Button size="small" theme="link">tiny!</Button>
+    </div>
+    <div>
+        <Button theme="text" size="big" >It's so big!</Button>
+        <Button theme="text">just normal.</Button>
+        <Button theme="text" size="small" >tiny!</Button>
     </div>
 </template>
 
