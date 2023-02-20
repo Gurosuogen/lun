55e4c87ab2fe11005fb8f3f0809e412f7e364c4c
gurosuogen <zcw5321@gmail.com>
2023-02-20 22:01:49 +0800

    使用 CSS 切换内容

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,13 +1,18 @@
 <script lang="ts">
+import { ref } from 'vue';
 import Tab from '../lib/Tab.vue';
 import Tabs from '../lib/Tabs.vue';
 export default {
     components: { Tab, Tabs },
+    setup() {
+        const x = ref('nav1')
+        return { x }
+    }
 }
 </script>
 <template>
-    <Tabs>
+    <Tabs v-model:selected="x">
         <Tab title="nav1">content1</Tab>
         <Tab title="nav2">content2</Tab>
-</Tabs>
+    </Tabs>
 </template>
