af35a1873c9ef3912dc2c80aabb55851bb91a414
gurosuogen <zcw5321@gmail.com>
2023-02-21 15:13:23 +0800

    修复 selectedItem.value is null

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 // @ts-nocheck
-import { computed, ref, watchEffect } from 'vue';
+import { computed, onMounted, ref, watchEffect } from 'vue';
 import Tab from './Tab.vue';
 export default {
     props: {
@@ -12,24 +12,26 @@ export default {
         const selectedItem = ref<HTMLDivElement>(null)
         const indicator = ref<HTMLDivElement>(null)
         const container = ref<HTMLDivElement>(null)
-
-        watchEffect(() => {
-            const { width } = selectedItem.value.getBoundingClientRect()
-            indicator.value.style.width = width + 'px'
-            const { left: left1 } = container.value.getBoundingClientRect()
-            const { left: left2 } = selectedItem.value.getBoundingClientRect()
-            const left = left2 - left1
-            indicator.value.style.left = left + 'px'
+        onMounted(() => {
+            watchEffect(() => {
+                const { width } = selectedItem.value.getBoundingClientRect()
+                indicator.value.style.width = width + 'px'
+                const { left: left1 } = container.value.getBoundingClientRect()
+                const { left: left2 } = selectedItem.value.getBoundingClientRect()
+                const left = left2 - left1
+                indicator.value.style.left = left + 'px'
+            })
         })
 
-        const defaults = context.slots.default!()
+
+        const defaults = context.slots.default()
         defaults.forEach((tag) => {
-            if (tag.type !== Tab) {
+            if (tag.type == Tab) {
                 throw new Error('Tabs 子标签必须是 Tab')
             }
         })
         const titles = defaults.map((tag) => {
-            return tag.props!.title
+            return tag.props.title
         })
         const select = (title: string) => {
             context.emit('update:selected', title)
@@ -48,7 +50,7 @@ export default {
         </div>
     </div>
     <div class="gulu-tabs-content">
-        <component class="gulu-tabs-content-item" :class="{ selected: c.props!.title === selected }" v-for="c in defaults"
+        <component class="gulu-tabs-content-item" :class="{ selected: c.props.title === selected }" v-for="c in defaults"
             :is="c"></component>
     </div>
 </template>
