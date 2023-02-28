efbcad32be1a6a921a3d6d0c3c1c36c77ceec140
gurosuogen <zcw5321@gmail.com>
2023-02-28 21:23:14 +0800

    配置部署

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import { md } from './plugins/md'
 import { demo } from './plugins/demo'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(), md(), demo()
-  ],
-})
+export default defineConfig(
+  {
+    base: './',
+    // assetsDir: 'assets',
+    plugins: [
+      vue(), md(), demo()
+    ],
+  })
